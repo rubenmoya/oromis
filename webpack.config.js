0a09a88067223bd2fd11c7b651d6b7e19b9ff0ea
rubenmoya <rmoyarodriguez@gmail.com>
2017-09-10 16:53:15 +0200

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'oromis.js',
+    library: 'Oromis',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['es2015'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new UglifyJSPlugin(),
+  ],
+};
